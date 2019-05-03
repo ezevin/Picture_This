bc7901e32ea2001bdeaa12662226cc96438a5708
ezevin <ehzevin@gmail.com>
2019-05-03 15:50:18 -0400

    controlling key strokes

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,9 @@
+//Arrow Key Info//
+// ArrowLeft = 37
+// ArrowRight = 39
+// ArrowUp = 38
+// ArrowDown = 40
+
 /************************************************/
 //        All Variables and Data                //
 /************************************************/
@@ -17,10 +23,21 @@ numbers.forEach(num => {
 })
 
 ////////////////Create A Keydown Event///////////
-document.addEventListener("click", function(e){
-  console.log("click")
+document.addEventListener("keydown", function(e){
+  // console.log("click")
   const movingTile = gameBoard.firstElementChild.lastElementChild
-  debugger
+  if(event.keyCode == 37) {
+       console.log('Left was pressed');
+  } else if(event.keyCode == 39) {
+        console.log('Right was pressed');
+  } else if(event.keyCode == 38) {
+        console.log('Up was pressed');
+  }else if(event.keyCode == 40) {
+        console.log('Down was pressed');
+  }else {
+    console.log("nope!")
+  }
+  // debugger
 })
 
 ///To Do List
